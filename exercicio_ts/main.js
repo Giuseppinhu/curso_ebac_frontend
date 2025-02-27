a34e6a92bfbd3de7711120e9688aec42c80a1599
Giuseppe <giuseppe.ribeiro.silva@gmail.com>
2025-02-27 19:18:00 -0400

    files typescript

---
+++ exercicio_ts/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const multiplier = (value, value2) => (value * value2);
+const sayHello = (name) => ("Hello! Developer " + name + " 🙂");
+console.log(`O resultado da multiplicação deu: ${multiplier(2, 2)}`);
+console.log('\n');
+console.log(sayHello("Giuseppe"));
---
+++ exercicio_ts/main.ts
@@ -0,0 +1,7 @@
+const multiplier = (value: number, value2:number) => (value * value2);
+
+const sayHello = (name: string) => ("Hello! Developer " + name + " 🙂");
+
+console.log(`O resultado da multiplicação deu: ${multiplier(2,2)}`)
+console.log('\n')
+console.log(sayHello("Giuseppe"))
